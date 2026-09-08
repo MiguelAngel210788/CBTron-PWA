b9dfb2708638c81996a18e14610d77846fde0500
MiguelAngel210788 <miguelangel210788@gmail.com>
2026-09-07 18:03:58 -0600

    chore: publish PWA version 0.1.23

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
  * y la guardará únicamente en este dispositivo.
  */
 window.CBTRON_PWA_CONFIG = Object.freeze({
-  version: '0.1.22',
+  version: '0.1.23',
   gasWebAppUrl: ''
 });
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CBTRON_PWA_CACHE = 'cbtron-pwa-0.1.22';
+const CBTRON_PWA_CACHE = 'cbtron-pwa-0.1.23';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(self.skipWaiting());
